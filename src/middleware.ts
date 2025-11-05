a43611f8dacf3980d81dd8a3ca27112a65e4414f
Iftekhar Uddin <iftekharuddin720@gmail.com>
2025-11-05 21:31:17 +0600

    change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,56 +1,39 @@
 import { NextResponse } from "next/server";
-import { auth } from "./auth";
+import { getToken } from "next-auth/jwt";
 import { apiAuthPrefix, authRoutes, publicRoutes, Default_Login_Redirect } from "./routes";
 
-// const { auth } = NextAuth(authConfig);
-
-export default auth((req) => {
+export async function middleware(req: any) {
   const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
-  const user = req.auth?.user;
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const isLoggedIn = !!token;
+  const userRole = token?.role;
+
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isAdminRoute = nextUrl.pathname.startsWith("/admin") || nextUrl.pathname.startsWith("/api/admin") || nextUrl.pathname.startsWith("/admin/jobs");
+  const isAdminRoute =
+    nextUrl.pathname.startsWith("/admin") ||
+    nextUrl.pathname.startsWith("/api/admin") ||
+    nextUrl.pathname.startsWith("/admin/jobs");
 
-  if (isApiAuthRoute) {
-    return null
-  };
+  if (isApiAuthRoute) return NextResponse.next();
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL(Default_Login_Redirect, nextUrl))
-    }
-    return null;
-  };
+  if (isAuthRoute && isLoggedIn)
+    return NextResponse.redirect(new URL(Default_Login_Redirect, nextUrl));
 
   if (!isLoggedIn && !isPublicRoute) {
-    let callbackUrl = nextUrl.pathname;
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search
-    }
-    const encoded = encodeURIComponent(nextUrl.pathname);
+    const encoded = encodeURIComponent(nextUrl.pathname + nextUrl.search);
     return NextResponse.redirect(
       new URL(`/auth/signin?callbackUrl=${encoded}`, nextUrl)
     );
   }
 
-
-  if (isAdminRoute) {
-    if (!isLoggedIn) {
-      const encoded = encodeURIComponent(nextUrl.pathname);
-      return NextResponse.redirect(
-        new URL(`/auth/signin?callbackUrl=${encoded}`, nextUrl)
-      );
-    };
-
-    if (user?.role !== "Admin") {
-      return NextResponse.redirect(new URL("/403", nextUrl));
-    }
+  if (isAdminRoute && userRole !== "Admin") {
+    return NextResponse.redirect(new URL("/403", nextUrl));
   }
-  return null;
 
-});
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
@@ -65,33 +48,63 @@ export const config = {
 
 
 
-// export { auth as middleware} from "@/lib/auth";
-
 
 // import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-// import { auth } from "@/lib/auth";
+// import { auth } from "./auth";
+// import { apiAuthPrefix, authRoutes, publicRoutes, Default_Login_Redirect } from "./routes";
 
+// export default auth((req) => {
+//   const { nextUrl } = req;
+//   const isLoggedIn = !!req.auth;
+//   const user = req.auth?.user;
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+//   const isAdminRoute = nextUrl.pathname.startsWith("/admin") || nextUrl.pathname.startsWith("/api/admin") || nextUrl.pathname.startsWith("/admin/jobs");
 
-// const protectedRoutes = ["/user-info"];
+//   if (isApiAuthRoute) {
+//     return null
+//   };
 
-// export default async function middleware(request: NextRequest){
-//   const session = await auth();
-//   const {pathname} = request.nextUrl;
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return NextResponse.redirect(new URL(Default_Login_Redirect, nextUrl))
+//     }
+//     return null;
+//   };
 
-//   const isProtected = protectedRoutes.some((route)=> pathname.startsWith(route));
-
-//   if(!session && !isProtected){
-//     return NextResponse.redirect(new URL("api/auth/signin", request.url))
+//   if (!isLoggedIn && !isPublicRoute) {
+//     let callbackUrl = nextUrl.pathname;
+//     if (nextUrl.search) {
+//       callbackUrl += nextUrl.search
+//     }
+//     const encoded = encodeURIComponent(nextUrl.pathname);
+//     return NextResponse.redirect(
+//       new URL(`/auth/signin?callbackUrl=${encoded}`, nextUrl)
+//     );
 //   }
 
-//   return NextResponse.next();
-// }
 
+//   if (isAdminRoute) {
+//     if (!isLoggedIn) {
+//       const encoded = encodeURIComponent(nextUrl.pathname);
+//       return NextResponse.redirect(
+//         new URL(`/auth/signin?callbackUrl=${encoded}`, nextUrl)
+//       );
+//     };
+
+//     if (user?.role !== "Admin") {
+//       return NextResponse.redirect(new URL("/403", nextUrl));
+//     }
+//   }
+//   return null;
+
+// });
 
 // export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// }
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
 
 
 
